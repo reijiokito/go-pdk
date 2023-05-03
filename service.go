eef5ee976e8f20f9d34a452e5db9269f84413a2a
congnt <congnt@gviet.vn>
2023-05-04 02:26:12 +0700

    update

--- service.go
+++ service.go
@@ -2,7 +2,9 @@ package go_pdk
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/nats-io/nats.go"
+	"google.golang.org/protobuf/encoding/protojson"
 	"google.golang.org/protobuf/proto"
 	"log"
 )
@@ -49,5 +51,60 @@ func RegisterService[R proto.Message](conn *nats.Conn, url string, handler Servi
 
 	if err != nil {
 		log.Fatal("Can not register service:", url)
+	}
+}
+
+var jsonMarshaller = protojson.MarshalOptions{
+	EmitUnpopulated: true,
+	UseProtoNames:   true,
+}
+
+type Service struct {
+	Context
+	Request    Request
+	Reply      string
+	Connection *nats.Conn
+}
+
+func (ctx *Service) Error(e *Error) {
+	response := Response{Code: 400}
+
+	var err error
+	if ctx.Request.JSON {
+		response.Body, err = json.Marshal(e)
+	}
+
+	if err != nil {
+		response.Code = int32(500)
+		response.Body = []byte(err.Error())
+	}
+	ctx.flush(&response)
+}
+
+func (ctx *Service) Done(r proto.Message) {
+	response := Response{Code: 200}
+
+	var err error
+	if ctx.Request.JSON {
+		response.Body, err = jsonMarshaller.Marshal(r)
+	} else {
+		response.Body, err = proto.Marshal(r)
+	}
+	if err != nil {
+		response.Code = int32(500)
+		response.Body = []byte(err.Error())
+	}
+	ctx.flush(&response)
+}
+
+func (ctx *Service) flush(response *Response) {
+	bytes, err := proto.Marshal(response)
+	if err != nil {
+		log.Print("Register marshal error response data:", err.Error())
+		return
+	}
+	err = ctx.Connection.Publish(ctx.Reply, bytes)
+	if err != nil {
+		log.Println(fmt.Sprintf("Nats publish to [%s] error: %s", ctx.Reply, err.Error()))
 	}
 }
